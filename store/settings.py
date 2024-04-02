165042e1dca3a8a3a6b591d8e2364c3b20d421b3
Radmir <galiullinradmir17@gmaul.com>
2024-04-02 19:22:26 +0300

    fix send_email bug

--- store/settings.py
+++ store/settings.py
@@ -36,6 +36,7 @@ env = environ.Env(
     EMAIL_HOST_PASSWORD=(str),
     EMAIL_USE_SSL=(bool),
 
+
     YOKASSA_SECRET=(str),
     YOKASSA_ACC_ID=(int),
     
@@ -215,14 +216,17 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 #Email
-if DEBUG:
-     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-else:
-    EMAIL_HOST =env('EMAIL_HOST')
-    EMAIL_PORT = env('EMAIL_PORT')
-    EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
-    EMAIL_USE_SSL = env('EMAIL_USE_SSL')
+# if DEBUG:
+#      EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# else:
+EMAIL_HOST =env('EMAIL_HOST')
+EMAIL_PORT = env('EMAIL_PORT')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_USE_SSL = env('EMAIL_USE_SSL')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
+EMAIL_ADMIN = EMAIL_HOST_USER
 
 #OAuth
 AUTHENTICATION_BACKENDS = [
