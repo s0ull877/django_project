8fe252833008048eef3301688a5df2c3cc4b260c
Radmir <galiullinradmir17@gmaul.com>
2024-05-03 16:44:44 +0300

    fix command

--- users/management/commands/create_superuser.py
+++ users/management/commands/create_superuser.py
@@ -23,6 +23,6 @@ class Command(BaseCommand):
 
     @info
     def handle(self, *args: Any, **options: Any) -> str | None:
-        self._class.create_superuser(
+        self._class.objects.create_superuser(
                 ADMIN_USERNAME, ADMIN_EMAIL, ADMIN_PASSWORD
         )
--- users/models.py
+++ users/models.py
@@ -11,6 +11,12 @@ class User(AbstractUser):
     image = models.ImageField(upload_to='user_images', null=True, blank=True)
     is_verified_email = models.BooleanField(default=False)
 
+    def create_superser(self, username:str, email:str, password:str ):
+        self.objects.save(
+            username=username, email=email, password=password, \
+            is_verified_email=True, is_staff=True, is_superuser=True
+            )
+
 
 class EmailVerification(models.Model):
 
